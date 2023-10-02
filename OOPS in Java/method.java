d8bd96ca0eded30c6c2013d35b4be4d478a1b3bd
Astik Mishra <astikmishra991@gmail.com>
2023-10-02 23:49:42 +0530

    Added all problems till 2 oct 2023

--- OOPS in Java/method.java
+++ OOPS in Java/method.java
@@ -1,5 +1,11 @@
+// Non Static Method
 public class method {
+    void exampleMethod(){
+        int n = 10;
+        System.out.println(n);
+    }
     public static void main(String[] args) {
-        
+        method mt = new method();
+        mt.exampleMethod();
     }    
 }
